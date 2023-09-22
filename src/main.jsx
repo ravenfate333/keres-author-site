132610d7a012cec874f196e619b4cbbcb56b08f5
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-09-22 13:56:26 -0400

    Update main.jsx
    deleted import './index.css/' as the file doesn't exist
    (possibly boilerplate the was deleted?)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
 //adding to implement react-router-dom
 import { createBrowserRouter, RouterProvider} from 'react-router-dom';
 
