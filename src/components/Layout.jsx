1e4daa2022ecc611bb382ad61cd0b363cf4d7292
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-09-27 16:20:37 -0400

    Update Layout.jsx
    added hamburger to Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom";
+import { Sling as Hamburger } from "hamburger-react";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import { useState } from "react";
 
 const Layout = () => {
+  const [isOpen, setOpen] = useState(false);
+
   return (
-    <div>
-      <Nav />
+    <>
+      <Hamburger toggled={isOpen} toggle={setOpen} />
+      {isOpen ? <Nav /> : null}
       <Outlet />
       <Footer />
-    </div>
+    </>
   );
 };
 
