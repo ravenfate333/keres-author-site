5aa7aa49239d908c163b40058c36343c74decc86
RavenFate333 <ravenfate333@gmail.com>
2023-09-27 13:44:14 -0600

    made some changes to navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,24 @@ import { Link } from "react-router-dom";
 function Nav() {
   return (
     <>
-    <nav>
+    <nav className="mobile-nav">
+      <ul className="nav-links">
+        <li>
+          <Link to={'/'}>HOME</Link>
+        </li>
+        <li>
+          <Link to={'/CrackedCoffins'}>THE CRACKED COFFINS SERIES</Link>
+        </li>
+        <li>
+          <Link to={'/Bio'}>BIO</Link>
+        </li>
+        <li>
+          <Link to={'/Contact'}>CONTACT</Link>
+        </li>
+      </ul>
+    </nav>
+
+    <nav className="desktop-nav">
       <ul className="nav-links">
         <li>
           <Link to={'/'}>HOME</Link>
