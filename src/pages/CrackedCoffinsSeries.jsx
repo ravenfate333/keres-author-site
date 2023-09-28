e0d9e82749b6bb9e764dbb2fa8cf19bec4a6c73f
RavenFate333 <ravenfate333@gmail.com>
2023-09-28 13:19:25 -0600

    added covers to page, removed default react svg

--- src/pages/CrackedCoffinsSeries.jsx
+++ src/pages/CrackedCoffinsSeries.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import "../styles/crackedcoffins.scss";
+import CrackedCoffins from "../assets/CrackedCoffinsCover.jpg";
+import BindingBlood from "../assets/BindingBloodCover.jpg";
+import HarrowedHearts from "../assets/HarrowedHeartsCover.jpeg";
 
 function CrackedCoffinsSeries() {
   return (
     <>
       <h1>Cracked Coffin Books Go Here</h1>
+      <div className="book">
+        <div className="cracked-coffins">
+          <img className="book-cover" src={CrackedCoffins} alt="Cracked Coffins Book Cover" />
+        </div>
+      </div>
+      <div className="book">
+        <div className="binding-blood">
+        <img className="book-cover" src={BindingBlood} alt="Binding Blood Book Cover" />
+        </div>
+      </div>
+      <div className="book">
+        <div className="harrowed hearts">
+        <img className="book-cover" src={HarrowedHearts} alt="Harrowed Hearts Book Cover" />
+        </div>
+      </div>
     </>
   );
 }
