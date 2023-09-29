47344c24b94ed56a0c408e85531bd7ac8a0eae92
RavenFate333 <ravenfate333@gmail.com>
2023-09-29 12:10:19 -0600

    filled bio page and did basic styling

--- src/pages/Bio.jsx
+++ src/pages/Bio.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import AuthorImage from "../assets/authorphoto.jpg";
+import "../styles/bio.scss";
 
 function Bio() {
   return (
     <>
       <div>
         <h1>BIO</h1>
+        <div className="bio-container">
+          <img
+            className="author-image"
+            src={AuthorImage}
+            alt="Photo of Beronika Keres"
+          />
+          <div className="author-bio">
+            <p>
+              Beronika Keres is the Canadian author of the dark fantasy thriller
+              series, Cracked Coffins. In the second grade, she decided she
+              wanted to be an author and has spent her life honing her craft and
+              pursuing her dream. She can often be found chasing plot bunnies
+              and writing books.
+            </p>
+            <p>
+              When she’s not writing, she enjoys spending time with her family,
+              or listening to some gothic rock, punk, or metal while working on
+              her newest spike and patch covered project.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
