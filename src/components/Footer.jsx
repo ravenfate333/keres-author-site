174977ca290886889125c345f8bb58073318f778
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-09-29 14:25:04 -0400

    Update Footer.jsx
    added styling (to be replaced later)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,27 @@
-import React from 'react'
-import SocialLinks from './SocialLinks'
+import React from "react";
+import SocialLinks from "./SocialLinks";
 
 function Footer() {
   return (
     <>
       <SocialLinks />
-      <p>
+      <p style={{ textAlign: "center" }}>
+        If you enjoy reading indie books, check out{" "}
+        <a style={{ color: "white" }} href="https://indiebookvault.com/">
+          Indie Book Vault
+        </a>{" "}
+        to find other great authors like Beronika Keres.
+      </p>
+      <p style={{ textAlign: "center" }}>
         Copyright Notice: All writings, blogs, books, and content within this
         site belong to Beronika Keres and are not permitted to be used without
         written permission.
       </p>
-      <p>
-        If you enjoy reading indie books, check out Indie Book Vault to find
-        other great authors like Beronika Keres.
-      </p>
-      <p>Area for Newsletter</p>
+      <div>
+        <p>Area for Newsletter</p>
+      </div>
     </>
   );
 }
 
-export default Footer
+export default Footer;
