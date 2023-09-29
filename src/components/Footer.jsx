3f9a3252abd72288a2bbc5c2c7c94455f5145bb1
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-09-29 12:56:53 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,18 @@ function Footer() {
   return (
     <>
       <SocialLinks />
-      <p>Copyright Notice?</p>
-      <p>Indie Book Vault notice</p>
+      <p>
+        Copyright Notice: All writings, blogs, books, and content within this
+        site belong to Beronika Keres and are not permitted to be used without
+        written permission.
+      </p>
+      <p>
+        If you enjoy reading indie books, check out Indie Book Vault to find
+        other great authors like Beronika Keres.
+      </p>
       <p>Area for Newsletter</p>
     </>
-  )
+  );
 }
 
 export default Footer
