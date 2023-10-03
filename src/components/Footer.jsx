171c3013d80ff31b1265ef3815d01d7565b79322
RavenFate333 <ravenfate333@gmail.com>
2023-10-02 20:27:45 -0600

    put footer in div

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,24 +5,26 @@ import "../styles/footer.scss";
 function Footer() {
   return (
     <>
-      <SocialLinks />
-      <p style={{ textAlign: "center" }}>
-        If you enjoy reading indie books, check out{" "}
-        <a style={{ color: "white" }} href="https://indiebookvault.com/">
-          Indie Book Vault
-        </a>{" "}
-        to find other great authors like Beronika Keres.
-      </p>
-      <p style={{ textAlign: "center" }}>
-        Copyright Notice: All writings, blogs, books, and content within this
-        site belong to Beronika Keres and are not permitted to be used without
-        written permission.
-      </p>
-      <div
-        className="ml-form-embed"
-        data-account="2083638:r3u1j5u3l4"
-        data-form="5983908:e0s3c3"
-      ></div>
+      <div className="footer">
+        <SocialLinks />
+        <p style={{ textAlign: "center" }}>
+          If you enjoy reading indie books, check out{" "}
+          <a style={{ color: "white" }} href="https://indiebookvault.com/">
+            Indie Book Vault
+          </a>{" "}
+          to find other great authors like Beronika Keres.
+        </p>
+        <p style={{ textAlign: "center" }}>
+          Copyright Notice: All writings, blogs, books, and content within this
+          site belong to Beronika Keres and are not permitted to be used without
+          written permission.
+        </p>
+        <div
+          className="ml-form-embed"
+          data-account="2083638:r3u1j5u3l4"
+          data-form="5983908:e0s3c3"
+        ></div>
+      </div>
     </>
   );
 }
