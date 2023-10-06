5728afeb0c9322450ec3f3143945214d15836142
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2023-10-06 15:25:00 -0400

    added a carousel
    - added a carousel without using a premade one due to issues found with available options of premade carousels

---
+++ src/components/carousel/Carousel2.jsx
@@ -0,0 +1,82 @@
+import React, { useEffect, useState } from "react";
+import "./carousel.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle } from "@fortawesome/free-solid-svg-icons";
+import SeriesFeature from "../../assets/images/SeriesFeature.webp";
+import HarrowedHeartsFeature from "../../assets/images/HarrowedHeartsFeature.webp";
+
+function Carousel2() {
+  // setting a state for when the user clicks the dots
+  const [clicked, setClicked] = useState(false);
+  // setting a state for default animation when not interacted by user
+  const [show, setShow] = useState(0);
+
+  const dot = <FontAwesomeIcon icon={faCircle} />;
+
+  // setting up useEffect to change the image every 5 seconds
+  useEffect(() => {
+    let timedChange;
+
+    if (!clicked) {
+      timedChange = setInterval(() => {
+        setShow((prevShow) => (prevShow === 0 ? 1 : 0));
+      }, 5000);
+    }
+
+    return () => clearInterval(timedChange);
+  }, [clicked]);
+
+  // setting a function to turn off timedChange and implement user selection
+  const userChange = () => {
+    setClicked(true);
+    if (show === 0) {
+      setShow(1);
+    } else {
+      setShow(0);
+    }
+  };
+
+  return (
+    <>
+      {show === 0 ? (
+        <>
+          <img className="hero-image" src={SeriesFeature} alt="book series" />
+          <p className="dots-box">
+            <span className="dots" style={{ color: "rgb(85, 96, 107" }}>
+              {dot}
+            </span>{" "}
+            <span
+              onClick={userChange}
+              className="dots"
+              style={{ color: "rgb(151, 162, 174)" }}
+            >
+              {dot}
+            </span>
+          </p>
+        </>
+      ) : (
+        <>
+          <img
+            className="hero-image"
+            src={HarrowedHeartsFeature}
+            alt="book preorder announcement"
+          />
+          <p className="dots-box">
+            <span
+              onClick={userChange}
+              className="dots"
+              style={{ color: "rgb(151, 162, 174)" }}
+            >
+              {dot}
+            </span>{" "}
+            <span className="dots" style={{ color: "rgb(85, 96, 107" }}>
+              {dot}
+            </span>
+          </p>
+        </>
+      )}
+    </>
+  );
+}
+
+export default Carousel2;
