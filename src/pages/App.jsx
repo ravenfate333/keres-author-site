ea87eb7c64568c7da4623d387e0ce43185f48096
RavenFate333 <ravenfate333@gmail.com>
2023-10-07 14:17:35 -0600

    removed original carousel

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Carousel from "../components/Carousel";
 import SeriesFeature from "../assets/SeriesFeature.png";
 import HarrowedHeartsFeature from "../assets/HarrowedHeartsFeature.png";
 import Carousel2 from "../components/carousel/Carousel2";
@@ -19,7 +18,6 @@ function App() {
       <div>
         <div>
           <Carousel2 />
-          <Carousel items={items} />
         </div>
       </div>
       <div>put more here</div>
