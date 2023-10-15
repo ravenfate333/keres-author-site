f816a8147c2bf893d99885f38bbb8a017909ea92
RavenFate333 <ravenfate333@gmail.com>
2023-10-15 00:11:07 -0600

    added reviews to homepage

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Carousel from "../components/Carousel";
 import "../styles/app.scss";
+import Review1 from "../assets/images/Review1.webp";
+import Review2 from "../assets/images/Review2.webp";
+import Review3 from "../assets/images/Review3.webp";
 
 function App() {
   return (
@@ -8,6 +11,23 @@ function App() {
       <div>
         <div className="carousel-container">
           <Carousel />
+        </div>
+        <div className="reviews-container">
+          <h1>WHAT READERS ARE SAYING ABOUT CRACKED COFFINS</h1>
+          <div className="reviews">
+            <img
+              src={Review1}
+              alt="Image of Cracked Coffins review."
+            />
+            <img
+              src={Review2}
+              alt="Image of Cracked Coffins review."
+            />
+            <img
+              src={Review3}
+              alt="Image of Cracked Coffins review."
+            />
+          </div>
         </div>
       </div>
     </>
